32406b05c1e72e764ccce2bb6fcd0ac4c52e4dcc
DaniloM05 <danilomiranda0505@gmail.com>
2023-11-24 11:37:23 -0300

    ultimos ajustes

--- generos-textuais/script.js
+++
@@ -1,9 +0,0 @@
-const ratoAvisoUp = window.document.querySelector("div.rato-Aviso-up")
-
-setTimeout(() => {
-    ratoAvisoUp.classList.add("--rato-Aviso-up-on")
-}, 1000)
-
-ratoAvisoUp.addEventListener("click", () => {
-    ratoAvisoUp.classList.remove("--rato-Aviso-up-on")
-})
